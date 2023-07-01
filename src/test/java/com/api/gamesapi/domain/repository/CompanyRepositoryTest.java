76fae2be7f6021abbbf0a1559747f23e2154ccfd
Versart <wesleyversart@gmail.com>
2023-06-30 22:10:40 -0300

    criando um novo application properties para testes

--- src/test/java/com/api/gamesapi/domain/repository/CompanyRepositoryTest.java
+++ src/test/java/com/api/gamesapi/domain/repository/CompanyRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class CompanyRepositoryTest {
 
     @Autowired
