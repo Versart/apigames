da6b5b2425d6a0ae19cb36706b915c5f57eb8645
Versart <wesleyversart@gmail.com>
2023-07-07 12:24:18 -0300

    adicionando anotação schema nos campos da companyDTO

--- src/main/java/com/api/gamesapi/api/model/CompanyDTO.java
+++ src/main/java/com/api/gamesapi/api/model/CompanyDTO.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,9 +17,11 @@ public class CompanyDTO {
 
     @NotNull
     @NotBlank
+    @Schema(description = "This is company's name", example = "Nintendo")
     private String name;
 
     @NotNull
+    @Schema(description = "This is the founding date of the company", example = "1889-09-23")
     private LocalDate dateOfFoundation;
 
 
