86afcce7371f5e649090378caccdc5ce9b681bf0
Versart <wesleyversart@gmail.com>
2023-07-09 17:28:51 -0300

    ordenando os métodos dos endpoints e adicionando os possiveis responses na documentação

--- src/main/java/com/api/gamesapi/api/controller/CompanyController.java
+++ src/main/java/com/api/gamesapi/api/controller/CompanyController.java
@@ -8,6 +8,7 @@ import com.api.gamesapi.domain.service.CompanyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -36,42 +37,58 @@ public class CompanyController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Saves a company",tags = "Company")
+    @Operation(summary = "Saves a company",tags = "Company",
+        responses = {@ApiResponse(description = "Successful operation", responseCode = "201"),
+                     @ApiResponse(description = "When the body is invalid", responseCode = "400")})
     public EntityModel<CompanyResponse> saveCompany(@Valid @RequestBody CompanyRequest companyDTO) {
         return companyService.saveCompany(companyDTO);
     }
 
     @GetMapping
-    @Operation(summary = "Lists all companies paginated" ,tags = "Company")
+    @Operation(summary = "Lists all companies paginated" ,tags = "Company", responses = {
+        @ApiResponse(description = "Successful operation", responseCode = "200")
+    })
     public ResponseEntity<PagedModel<EntityModel<CompanyResponse>>> listCompanies(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(companyService.listCompanies(pageable));
     }
 
     @GetMapping("/{companyId}")
-    @Operation(summary = "Gets a company by id",tags = "Company")
-    public ResponseEntity<EntityModel<CompanyResponse>> getCompanyById(@Schema(description = "this is company's id", type = "long") @PathVariable long companyId) {
+    @Operation(summary = "Gets a company by id",tags = "Company", responses = {
+        @ApiResponse(description = "Successful operation", responseCode = "200"),
+        @ApiResponse(description = "When the company does not exists in the database", responseCode = "404")})
+    public ResponseEntity<EntityModel<CompanyResponse>> getCompanyById(@Schema(description = "this is company's id", type = "number") @PathVariable long companyId) {
         return ResponseEntity.ok(companyService.searchCompanyById(companyId));
     }
     @GetMapping("/find")
-    @Operation(summary = "Gets companies which contains in the name, the name searched",tags = "Company")
-    public ResponseEntity<CollectionModel<EntityModel<CompanyResponse>>> getByName(@Schema(description = "this is company's name", type = "long") @RequestParam String name){
+    @Operation(summary = "Gets companies which contains in the name, the name searched",tags = "Company"
+    ,responses = {@ApiResponse(description = "Successful operation", responseCode = "200")})
+    public ResponseEntity<CollectionModel<EntityModel<CompanyResponse>>> getByName(@Schema(description = "this is company's name", type = "string") @RequestParam String name){
         return ResponseEntity.ok(companyService.findCompanyByName(name));
     }
 
     @GetMapping("/{companyId}/games")
-    @Operation(summary = "Lists all games of company",tags = "Company")
-    public ResponseEntity<CollectionModel<EntityModel<GameResponseDTO>>> getGamesByCompanyId(@Schema(description = "this is company's id", type = "long") @PathVariable Long companyId ){
+    @Operation(summary = "Lists all games of company",tags = "Company", responses = {
+        @ApiResponse(description = "Successful operation", responseCode = "200"),
+        @ApiResponse(description = "When the company does not exists in the database", responseCode = "404")})
+    public ResponseEntity<CollectionModel<EntityModel<GameResponseDTO>>> getGamesByCompanyId(@Schema(description = "this is company's id", type = "number") @PathVariable Long companyId ){
        return ResponseEntity.ok(companyService.getGamesOfCompany(companyId));
     }
 
     @PutMapping("/{companyId}")
-    @Operation(summary = "Alters a company",tags = "Company")
-    public ResponseEntity<EntityModel<CompanyResponse>> updateCompanyById(@Schema(description = "this is company's id", type = "long")@PathVariable long companyId, @Valid @RequestBody CompanyRequest companyDTO){
+    @Operation(summary = "Alters a company",tags = "Company", responses = {
+        @ApiResponse(description = "Successful operation", responseCode = "200"),
+        @ApiResponse(description = "When the company does not exists in the database", responseCode = "404")
+    })
+    public ResponseEntity<EntityModel<CompanyResponse>> updateCompanyById(@Schema(description = "this is company's id", type = "number")@PathVariable long companyId, @Valid @RequestBody CompanyRequest companyDTO){
         return ResponseEntity.ok(companyService.updateCompanyById(companyId,companyDTO));
     }
+
     @DeleteMapping("/{companyId}")
-    @Operation(summary = "Deletes a company by id",tags = "Company")
-    public ResponseEntity<Void> deleteCompanyById(@Schema(description = "this is company's id", type = "long") @PathVariable long companyId){
+    @Operation(summary = "Deletes a company by id",tags = "Company", responses = {
+        @ApiResponse(description = "Successful operation", responseCode = "204"),
+        @ApiResponse(description = "When the company does not exists in the database", responseCode = "404")
+    })
+    public ResponseEntity<Void> deleteCompanyById(@Schema(description = "this is company's id", type = "number") @PathVariable long companyId){
         companyService.deleteCompanyById(companyId);
         return ResponseEntity.noContent().build();
     }
