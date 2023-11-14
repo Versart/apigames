4f1053a835b3f1303f755dde55a6eb510e4c4519
Versart <wesleyversart@gmail.com>
2023-11-14 14:06:05 -0300

    alterando o método findByLogin para retornar um optional

--- src/main/java/com/api/gamesapi/domain/repository/UserRepository.java
+++ src/main/java/com/api/gamesapi/domain/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.api.gamesapi.domain.repository;
 
 import com.api.gamesapi.domain.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    UserDetails findByLogin(String login);
+    Optional<UserDetails> findByLogin(String login);
 
     boolean existsUserByLogin(String login);
 }
