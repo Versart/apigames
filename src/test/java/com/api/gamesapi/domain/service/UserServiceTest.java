48b5e9607f3fb4fc62e927d2a888a0c2b1ad7e06
Versart <wesleyversart@gmail.com>
2023-11-17 18:29:41 -0300

    Adicionando mais testes na userService

--- src/test/java/com/api/gamesapi/domain/service/UserServiceTest.java
+++ src/test/java/com/api/gamesapi/domain/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.api.gamesapi.domain.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -11,11 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.api.gamesapi.api.mapper.UserMapper;
 import com.api.gamesapi.api.model.LoginRequest;
 import com.api.gamesapi.api.model.UserRequest;
 import com.api.gamesapi.api.model.UserResponse;
+import com.api.gamesapi.domain.exception.DuplicatedEmailException;
 import com.api.gamesapi.domain.model.User;
 import com.api.gamesapi.domain.repository.UserRepository;
 import com.api.gamesapi.infra.security.TokenService;
@@ -30,38 +33,67 @@ class UserServiceTest {
     private UserService userService;
 
     @Mock
-    private UserRepository userRepository;
+    private UserRepository userRepositoryMock;
 
     @Mock
-    private UserMapper userMapper;
+    private UserMapper userMapperMock;
 
     @Mock
-    private AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManagerMock;
 
     @Mock
-    private Authentication authentication;
+    private Authentication authenticationMock;
 
     @Mock
-    private TokenService tokenService;
+    private TokenService tokenServiceMock;
 
     @BeforeEach
     void setup() {
         String token = "sahsgahsg";
 
-        BDDMockito.when(userMapper.toEntity(ArgumentMatchers.any(UserRequest.class)))
+        BDDMockito.when(userMapperMock.toEntity(ArgumentMatchers.any(UserRequest.class)))
             .thenReturn(UserCreator.createUserAdmin());
-        BDDMockito.when(userMapper.toModel(ArgumentMatchers.any(User.class)))
+        BDDMockito.when(userMapperMock.toModel(ArgumentMatchers.any(User.class)))
             .thenReturn(UserDTOCreator.createUserResponse());
-        BDDMockito.when(userRepository.save(ArgumentMatchers.any(User.class)))
+        BDDMockito.when(userRepositoryMock.save(ArgumentMatchers.any(User.class)))
             .thenReturn(UserCreator.createUserAdmin());
-        BDDMockito.when(authenticationManager.authenticate(ArgumentMatchers.any(Authentication.class)))
-            .thenReturn(authentication);
-        BDDMockito.when(authentication.getPrincipal()).thenReturn(UserCreator.createUserAdmin());
-        BDDMockito.when(tokenService.getToken(ArgumentMatchers.any(User.class)))
+        BDDMockito.when(authenticationManagerMock.authenticate(ArgumentMatchers.any(Authentication.class)))
+            .thenReturn(authenticationMock);
+        BDDMockito.when(authenticationMock.getPrincipal()).thenReturn(UserCreator.createUserAdmin());
+        BDDMockito.when(tokenServiceMock.getToken(ArgumentMatchers.any(User.class)))
             .thenReturn(token);
+        BDDMockito.when(userRepositoryMock.existsUserByLogin(ArgumentMatchers.anyString()))
+            .thenReturn(false);
     }
 
     @Test
+    @DisplayName("loginExists returns true when User exists")
+    void loginExists_ReturnsTrue_WhenUserExists() {
+         BDDMockito.when(userRepositoryMock.existsUserByLogin(ArgumentMatchers.anyString()))
+            .thenReturn(true);
+        String login = LoginCreator.createLoginRequest().getLogin();
+
+        boolean exists = userService.loginxists(login);
+
+        Assertions.assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("loginExists returns false when User not exists")
+    void loginExists_ReturnsFalse_WhenUserExists() {
+       
+
+        String login = "";
+
+        boolean exists = userService.loginxists(login);
+
+        Assertions.assertThat(exists).isFalse();
+
+    }
+
+
+    @Test
+    @DisplayName("saveUser returns UserResponse when successful")
     void saveUser_ReturnsUserResponse_WhenSuccessful() {
         UserRequest userToBeSaved = UserDTOCreator.createUserRequest();
 
@@ -71,8 +103,22 @@ class UserServiceTest {
 
         Assertions.assertThat(userSaved.getLogin()).isEqualTo(userToBeSaved.getLogin());
     }
+
+    @Test
+    @DisplayName("saveUser throws DuplicatedEmailException when where there is already user with email")
+    void saveUser_ThrowsDuplicatedEmailException_WhenThereIsAlreadyUserWithEmail() {
+        BDDMockito.when(userRepositoryMock.existsUserByLogin(ArgumentMatchers.anyString()))
+            .thenReturn(true);
+        
+        UserRequest userRequest = UserDTOCreator.createUserRequest();
+
+        Assertions.assertThatExceptionOfType(DuplicatedEmailException.class)
+            .isThrownBy(() -> userService.saveUser(userRequest));
+    }
+
     
     @Test
+    @DisplayName("login returns Token when successful")
     void login_ReturnsToken_WhenSuccessful() {
         LoginRequest loginRequest = LoginCreator.createLoginRequest();
 
@@ -80,5 +126,24 @@ class UserServiceTest {
 
         Assertions.assertThat(token).isNotNull().isNotEmpty();
     }
+
+    @Test
+    @DisplayName("login throws AuthenticationException when user is not found")
+    void login_ThrowsAuthenticationException_WhenUserIsNotFound() {
+        BDDMockito.when(authenticationManagerMock.authenticate(ArgumentMatchers.any(Authentication.class)))
+            .thenThrow(AuthenticationFailed.class);
+
+        LoginRequest loginRequest = LoginCreator.createLoginRequest();
+
+        Assertions.assertThatExceptionOfType(AuthenticationFailed.class)
+            .isThrownBy(() -> userService.login(loginRequest));
+    }
+
+    class AuthenticationFailed extends AuthenticationException{
+        public AuthenticationFailed(String msg){
+            super(msg);
+        }
+    }
+
 
 }
