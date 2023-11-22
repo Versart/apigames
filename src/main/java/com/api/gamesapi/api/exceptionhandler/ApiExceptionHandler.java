90c5227083ec52780089b92ac443b0676c3f01f2
Versart <wesleyversart@gmail.com>
2023-11-22 14:52:14 -0300

    Resolvendo erro do exceptionHandler não capturar exceção do tipo AuthenticationException

--- src/main/java/com/api/gamesapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/api/gamesapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,9 +4,7 @@ import com.api.gamesapi.domain.exception.CompanyNotFoundException;
 import com.api.gamesapi.domain.exception.DuplicatedEmailException;
 import com.api.gamesapi.domain.exception.NotFoundException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,12 +20,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.sasl.AuthenticationException;
 
 @ControllerAdvice
 @RequiredArgsConstructor
