bb0b4eff45a5328b83b822ea36005b24b0789286
Versart <wesleyversart@gmail.com>
2023-11-22 14:55:38 -0300

    Criando mais testes na AuthenticationControllerIT e adicionando dependência do org apache client 5

--- src/test/java/com/api/gamesapi/integration/AuthenticationControllerIT.java
+++ src/test/java/com/api/gamesapi/integration/AuthenticationControllerIT.java
@@ -1,25 +1,40 @@
 package com.api.gamesapi.integration;
 
+
+
+import java.io.IOException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.ResponseErrorHandler;
 import com.api.gamesapi.api.model.LoginRequest;
 import com.api.gamesapi.api.model.UserRequest;
 import com.api.gamesapi.api.model.UserResponse;
+import com.api.gamesapi.domain.model.User;
 import com.api.gamesapi.domain.repository.UserRepository;
 import com.api.gamesapi.domain.service.UserService;
 import com.api.gamesapi.infra.security.TokenService;
@@ -33,6 +48,7 @@ import com.api.gamesapi.util.LoginCreator;
 class AuthenticationControllerIT {
     
     @Autowired
+    @Qualifier(value = "testRestTemplate")
     private TestRestTemplate testRestTemplate;
 
     @Autowired
@@ -45,7 +61,19 @@ class AuthenticationControllerIT {
     private TokenService tokenService;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository;    
+
+    @TestConfiguration()
+    @Lazy
+    static class RestTemplateBuilderConfiguration {
+        @Bean(name = "testRestTemplate")
+        TestRestTemplate restTemplateBuilder(@Value("${local.server.port}") int port) {
+            return new TestRestTemplate(new RestTemplateBuilder().errorHandler(new DefaultResponseErrorHandler()
+            )
+            .requestFactory(HttpComponentsClientHttpRequestFactory.class)
+            .rootUri("http://localhost:" + port));
+        }
+    }
 
     @Test
     @DisplayName("login returns token when successful")
@@ -66,6 +94,24 @@ class AuthenticationControllerIT {
     }
 
     @Test
+    @DisplayName("login throws unauthorized when login fails")
+    void login_ThrowsUnauthorized_WhenLoginFails() {
+        LoginRequest loginRequest = LoginCreator.createLoginRequest();
+        
+        String url = "/auth/login";
+        
+        ResponseEntity<String> exchange = testRestTemplate.exchange(url, HttpMethod.POST, 
+        new HttpEntity<>(loginRequest), new ParameterizedTypeReference<>() {   
+        });
+      
+        Assertions.assertThat(exchange).isNotNull();
+
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+    }
+
+
+    @Test
     @DisplayName("register returns UserResponse when successful")
     void register_ReturnsUserResponse_WhenSuccessful() {
         String url = "/auth/register";
@@ -84,6 +130,29 @@ class AuthenticationControllerIT {
 
         Assertions.assertThat(exchange.getBody().getLogin()).isEqualTo(userToBeSaved.getLogin());
     }
+
+    @Test
+    @DisplayName("register throws DuplicatedEmailException when there is already user with email")
+    void register_ThrowsDuplicatedEmailException_WhenThereIsAlreadyUserWithEmail() {
+        String url = "/auth/register";
+        
+        UserRequest userToBeSaved = UserDTOCreator.createUserRequest();
+
+        User user = UserCreator.createUserAdmin();
+        
+        userRepository.save(user);
+
+        ResponseEntity<UserResponse> exchange = testRestTemplate.exchange(url, HttpMethod.POST,
+             new HttpEntity<>(userToBeSaved), new ParameterizedTypeReference<>() {
+                
+             });
+        
+        Assertions.assertThat(exchange).isNotNull();
+
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+
+    }
+
 
     
     @AfterEach
