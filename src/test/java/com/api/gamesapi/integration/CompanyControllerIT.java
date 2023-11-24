5662b3c7d10688aeed3759a7bbe0a3c0cea4300e
Versart <wesleyversart@gmail.com>
2023-11-24 15:20:32 -0300

    Adicionando mais testes de integração na CompanyControllerIT

--- src/test/java/com/api/gamesapi/integration/CompanyControllerIT.java
+++ src/test/java/com/api/gamesapi/integration/CompanyControllerIT.java
@@ -20,7 +20,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.*;
@@ -86,8 +85,34 @@ class CompanyControllerIT {
     }
 
     @Test
-    @DisplayName("getCompanyById returns model of companyDTO when successful")
-    void getCompanyById_ReturnEntityModelOfCompanyDto_WhenSuccessfull() {
+    @DisplayName("listCompanies returns empty PagedModel of CompanyResponse when successful")
+    void listCompanies_ReturnEmptysPageModelOfEntityModelCompanyDTO_WhenNoCompanyIsFound() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        String url = "/companies";
+
+        ResponseEntity<PagedModel<EntityModel<CompanyResponse>>>
+                exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(
+                        null,httpHeaders
+                ),
+                new ParameterizedTypeReference<>(){
+                });
+
+        Assertions.assertThat(exchange).isNotNull();
+
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        
+        Assertions.assertThat(exchange.getBody().getContent()).isNotNull().isEmpty();;
+         
+        Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("getCompanyById returns EntityModel of CompanyResponse when successful")
+    void getCompanyById_ReturnsEntityModelOfCompanyResponse_WhenSuccessfull() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -104,6 +129,7 @@ class CompanyControllerIT {
                 }, idCompanyExpected);
 
         Assertions.assertThat(exchange).isNotNull();
+
         Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         Assertions.assertThat(exchange.getBody()).isNotNull();
@@ -112,14 +138,37 @@ class CompanyControllerIT {
 
         Assertions.assertThat(exchange.getBody().getContent().getId()).isEqualTo(idCompanyExpected);
         
-        Assertions.assertThat(exchange.getBody().getLinks()).isNotEmpty().isNotNull();
-        
         Assertions.assertThat(exchange.getBody().getLinks().toString()).contains(url.replace("/{id}","") + "/"+ idCompanyExpected);
+    
     }
 
     @Test
-    @DisplayName("GetByName returns a collection model of companyDTO when successful")
-    void getByName_ReturnCollectionModelOfCompanyDto_WhenSuccessful() {
+    @DisplayName("getCompanyById throws NotFoundException when Company is not found")
+    void getCompanyById_ThrowsNotFoundException_WhenCompanyIsNotFound() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        Long idCompanyExpected = 1l;
+        String url = "/companies/{id}";
+
+        ResponseEntity<Object>
+                exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(
+                        null,httpHeaders
+                )
+                , new ParameterizedTypeReference<>() {
+                }, idCompanyExpected);
+
+        Assertions.assertThat(exchange).isNotNull();
+
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        
+    }
+
+    @Test
+    @DisplayName("GetByName returns  PagedModel of CompanyResponse when successful")
+    void getByName_ReturnsPagedModelOfCompanyResponse_WhenSuccessful() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -128,7 +177,7 @@ class CompanyControllerIT {
         Company companySaved = companyRepository.save(CompanyCreator.createCompanyToBeSaved());
         String nameExpected = companySaved.getName();
         String url = String.format("/companies/find?name=%s", nameExpected);
-        ResponseEntity<CollectionModel<EntityModel<CompanyRequest>>> exchange = testRestTemplate
+        ResponseEntity<PagedModel<EntityModel<CompanyRequest>>> exchange = testRestTemplate
                 .exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders),
                         new ParameterizedTypeReference<>() {
                         });
@@ -149,11 +198,13 @@ class CompanyControllerIT {
         
         Assertions.assertThat(exchange.getBody().getContent().stream().toList()
                 .get(0).getLinks().toString()).contains("/companies/" + companySaved.getId());
+        
+        Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
     }
 
     @Test
-    @DisplayName("getByName returns an empty collection model of companyDTO when company is not found")
-    void getByName_ReturnEmptyCollectionModelOfCompanyDto_WhenCompanyIsNotFound() {
+    @DisplayName("GetByName returns empty PagedModel of CompanyResponse when no Company is found")
+    void getByName_ReturnsEmptyPagedModelOfCompanyDto_WhenNoCompanyIsFound() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -161,15 +212,20 @@ class CompanyControllerIT {
 
         String nameExpected = "";
         String url = String.format("/companies/find?name=%s", nameExpected);
-        ResponseEntity<CollectionModel<EntityModel<CompanyRequest>>> exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders), new ParameterizedTypeReference<>() {
-        });
+        ResponseEntity<PagedModel<EntityModel<CompanyRequest>>> exchange = testRestTemplate
+                .exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders),
+                        new ParameterizedTypeReference<>() {
+                        });
 
         Assertions.assertThat(exchange).isNotNull();
         
         Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
         
         Assertions.assertThat(exchange.getBody()).isNotNull().isEmpty();
-
+        
+        Assertions.assertThat(exchange.getBody().getContent()).isNotNull().isEmpty();
+        
+        Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
     }
 
     @Test
@@ -197,12 +253,31 @@ class CompanyControllerIT {
         Assertions.assertThat(exchange.getBody().getContent().getId()).isNotNull();
         
         Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
-
     }
 
     @Test
-    @DisplayName("UpdateCompanyById returns a altered companyDTO")
-    void updateCompanyById_ReturnEntityModelOfCompanyDto_WhenSuccessful() {
+    @DisplayName("saveCompany throws BadRequest when Company do not have a name")
+    void saveCompany_ThrowsBadRequest_WhenCompanyDoNotHaveAName() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        CompanyRequest companyToBeSaved = CompanyDTOCreator.createCompanyRequestWithoutName();
+        String url = "/companies";
+        ResponseEntity<Object> exchange = testRestTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(companyToBeSaved,httpHeaders),
+                new ParameterizedTypeReference<>() {
+                });
+
+        Assertions.assertThat(exchange).isNotNull();
+        
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        
+    }
+
+    @Test
+    @DisplayName("UpdateCompanyById returns EntityModel of CompanyResponse")
+    void updateCompanyById_ReturnEntityModelOfCompanyResponse_WhenSuccessful() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -212,6 +287,7 @@ class CompanyControllerIT {
         String url = "/companies/{id}";
         CompanyRequest companyToUpdated = CompanyDTOCreator.createCompanyRequest();
         Long idUpdated = companySaved.getId();
+        
         ResponseEntity<EntityModel<CompanyRequest>> exchange = testRestTemplate.exchange(url, HttpMethod.PUT, new HttpEntity<>(companyToUpdated,httpHeaders),
                 new ParameterizedTypeReference<>() {
                 }, idUpdated);
@@ -226,13 +302,33 @@ class CompanyControllerIT {
         
         Assertions.assertThat(exchange.getBody().getContent().getDateOfFoundation()).isEqualTo(companyToUpdated.getDateOfFoundation());
         
-        Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
-        
         Assertions.assertThat(exchange.getBody().getLinks().toString()).contains("/companies/" + idUpdated);
     }
 
     @Test
-    void getGamesByCompanyId_ReturnCollectionEntityModelOfGamesResponse_WhenSuccessful() {
+    @DisplayName("UpdateCompanyById throws NotFoundException when Company is not found")
+    void updateCompanyById_ThrowsNotFoundException_WhenCompanyIsNotFound() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        String url = "/companies/{id}";
+        CompanyRequest companyToUpdated = CompanyDTOCreator.createCompanyRequest();
+        Long idUpdated = 1l;
+        
+        ResponseEntity<EntityModel<CompanyRequest>> exchange = testRestTemplate.exchange(url, HttpMethod.PUT, new HttpEntity<>(companyToUpdated,httpHeaders),
+                new ParameterizedTypeReference<>() {
+                }, idUpdated);
+
+        Assertions.assertThat(exchange).isNotNull();
+        
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        
+    }
+    @Test
+    @DisplayName("getGamesByCompanyId returns PagedModel of GameResponse when successful")
+    void getGamesByCompanyId_ReturnsPagedModelOfGamesResponse_WhenSuccessful() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -245,7 +341,7 @@ class CompanyControllerIT {
         String url = "/companies/{id}/games";
         Long idCompany = companySaved.getId();
 
-        ResponseEntity<CollectionModel<EntityModel<GameResponseDTO>>> exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders),
+        ResponseEntity<PagedModel<EntityModel<GameResponseDTO>>> exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders),
                 new ParameterizedTypeReference<>() {
                 }, idCompany);
         
@@ -259,16 +355,43 @@ class CompanyControllerIT {
         
         Assertions.assertThat(exchange.getBody().getContent().stream().toList().get(0).getContent()).isNotNull();
         
-        Assertions.assertThat(exchange.getBody().getContent().stream().toList().get(0).getContent().getName()
-            ).isEqualTo(gameSaved.getName());
-        
+        Assertions.assertThat(exchange.getBody().getContent().stream().toList().get(0).getContent().getId()
+            ).isEqualTo(gameSaved.getId());
+ 
         Assertions.assertThat(exchange.getBody().getContent().stream().toList().get(0).getLinks()).isNotNull();
         
         Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
     }
 
     @Test
-    @DisplayName("deleteCompanyById removes companyDTO when successful")
+    @DisplayName("getGamesByCompanyId returns empty PagedModel of GameResponse when Company does not have games")
+    void getGamesByCompanyId_ReturnsEmptyPagedModelOfGamesResponse_WhenSuccessful() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        Company companySaved = companyRepository.save(CompanyCreator.createCompanyToBeSaved());
+        String url = "/companies/{id}/games";
+        Long idCompany = companySaved.getId();
+
+        ResponseEntity<PagedModel<EntityModel<GameResponseDTO>>> exchange = testRestTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(null,httpHeaders),
+                new ParameterizedTypeReference<>() {
+                }, idCompany);
+        
+        Assertions.assertThat(exchange).isNotNull();
+        
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        
+        Assertions.assertThat(exchange.getBody()).isNotNull();
+        
+        Assertions.assertThat(exchange.getBody().getContent()).isNotNull().isEmpty();
+        
+        Assertions.assertThat(exchange.getBody().getLinks()).isNotNull().isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("deleteCompanyById removes Company when successful")
     void deleteCompanyById_DeleteCompany_WhenSuccessful() {
         LoginRequest loginRequest = new LoginRequest("maria","12345678");
         String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
@@ -284,7 +407,24 @@ class CompanyControllerIT {
         Assertions.assertThat(exchange).isNotNull();
         
         Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
 
+    @Test
+    @DisplayName("deleteCompanyById throws NotFoundException when Company is not found")
+    void deleteCompanyById_ThrowsNotFound_WhenCompanyIsNotFound() {
+        LoginRequest loginRequest = new LoginRequest("maria","12345678");
+        String token = testRestTemplate.postForObject("/auth/login",loginRequest,String.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer " + token);
+
+        String url = "/companies/{id}";
+        Long idToDelete = 1l;
+
+        ResponseEntity<Void> exchange = testRestTemplate.exchange(url, HttpMethod.DELETE, new HttpEntity<>(null,httpHeaders), Void.class, idToDelete);
+
+        Assertions.assertThat(exchange).isNotNull();
+        
+        Assertions.assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @BeforeEach
